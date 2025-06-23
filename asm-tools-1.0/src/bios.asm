07d1e562bca1a94680590e8c7b881afc3c2c4b0f
Naharashu <130159705+Naharashu@users.noreply.github.com>
2025-06-23 13:45:31 +0300

    Create bios.asm

---
+++ asm-tools-1.0/src/bios.asm
@@ -0,0 +1,29 @@
+[bits 16]
+putChar:
+	mov ah, 0x0e
+	int 0x10
+	ret
+
+putNL:
+	mov ah, 0x0e
+	mov al, 0x0D
+	int 0x10
+	mov al, 0x0A
+	int 0x10
+	ret
+
+print:
+	 mov ah, 0x0e
+.repeat:
+	lodsb
+	cmp AL, 0
+	je .done
+	int 10h
+	jmp .repeat
+.done:
+	ret
+
+writeln:
+	call print
+	call putNL
+	ret
